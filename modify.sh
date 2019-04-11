5a271844958a76fe4e583c527c80e3b167225827
durongze <durongze@qq.com>
2019-04-11 17:30:55 +0100

    串口调试命令

--- modify.sh
+++ modify.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 find ./ -iname "Makefile" | xargs -I {}  sed '/.*KERNELDIR=/{n;/EXTRA_FLAGS=/ba;s!.*!EXTRA_CFLAGS=-Dsimple_DEBUG\n&!;:a}' -i {} 
+
+echo ttyS0,115200 > /sys/module/kgdboc/parameters/kgdboc
+echo g > /proc/sysrq-trigger
+
+gdb vmlinux
+#  >set serial baud 115200
+#  >target remote /dev/ttyUSB0
