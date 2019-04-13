aaf0be9cc946d2a76ce04b9eecc79e8df36e4a76
durongze <durongze@qq.com>
2019-04-13 06:00:12 +0100

    config生成

--- modify.sh
+++ modify.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+modprobe configs
+zcat /proc/config.gz
+
 find ./ -iname "Makefile" | xargs -I {}  sed '/.*KERNELDIR=/{n;/EXTRA_FLAGS=/ba;s!.*!EXTRA_CFLAGS=-Dsimple_DEBUG\n&!;:a}' -i {} 
 
 echo ttyS0,115200 > /sys/module/kgdboc/parameters/kgdboc
