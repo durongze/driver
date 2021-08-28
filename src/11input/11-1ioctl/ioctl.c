b3372024e6b3644d6c530c5add7dc79cccc18605
duyongze <duyongzeyx@qq.com>
2021-08-28 10:45:55 +0800

    add ioctl demo

--- src/11input/11-1ioctl/ioctl.c
+++ src/11input/11-1ioctl/ioctl.c
@@ -1,12 +1,32 @@
-int main () 
+#include <stdint.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/input.h>
+#include <stdio.h>
+struct input_devinfo {
+    __u16 bustype;
+    __u16 vendor;
+    __u16 product;
+    __u16 version;
+};
+int main (int argc, char** argv) 
 {
     int version;
-    ioctl(fd, EVIOCGVERSION, &version)；//获取版本
+    if (argc != 2) {
+        printf("%s <dev>", argv[0]);
+        return 1;
+    }
+	int fd = open(argv[1], O_RDONLY);
+    if (-1 == fd) {
+        return 2;
+    }
+    ioctl(fd, EVIOCGVERSION, &version);
     struct input_devinfo device_info;
-    ioctl(fd, EVIOCGID, &device_info)；//获取设备信息
+    ioctl(fd, EVIOCGID, &device_info);
     char name[256]= "Unknown";
-    ioctl(fd, EVIOCGNAME(sizeof(name)), name);//获取名称
+    ioctl(fd, EVIOCGNAME(sizeof(name)), name);
     uint8_t rel_bitmask[REL_MAX/8 + 1];
-    ioctl(fd, EVIOCGBIT(EV_REL, sizeof(rel_bitmask));//获取支持的鼠标特性
+    ioctl(fd, EVIOCGBIT(EV_REL, sizeof(rel_bitmask)));
     return 0;
 }
