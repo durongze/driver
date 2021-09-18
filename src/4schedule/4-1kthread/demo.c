4aff965a86d16480463bcc03dba36064a9c0e02e
duyongze <duyongzeyx@qq.com>
2021-09-18 09:49:45 +0800

    add kthread

--- src/4schedule/4-1kthread/demo.c
+++ src/4schedule/4-1kthread/demo.c
@@ -14,15 +14,19 @@
 #include <linux/sched.h>
 #include <linux/kthread.h> 
 
+#define PDEBUG(fmt, args...) printk("[%s:%d]" fmt, __FUNCTION__, __LINE__, ## args)
 
 static struct task_struct *simple_thread;
+
 int threadfunc(void *data)
 {
-	while(1)
-	{
+	while(1) {
 		set_current_state(TASK_UNINTERRUPTIBLE);
-		if(kthread_should_stop())break;
-		printk("threadfunc\n");
+		if (kthread_should_stop()) {
+			PDEBUG(" stop...\n");
+			break;
+		}
+		PDEBUG(" running...\n");
 		schedule_timeout(HZ);
 	}
 	return 0;
@@ -30,24 +34,25 @@ int threadfunc(void *data)
 
 void simple_cleanup_module(void)
 {
-	if(simple_thread)
-	{
+	if(simple_thread) {
 		kthread_stop(simple_thread);
 		simple_thread = NULL;
 	}
+	PDEBUG(" cleanup succ.\n");
 }
 
 int simple_init_module(void)
 {
 	int err;
     simple_thread = kthread_run(threadfunc, NULL, "simple_thread");
-    if(IS_ERR(simple_thread))
-	{
-		printk("kthread_create failed.\n");
+    if (IS_ERR(simple_thread)) {
+		PDEBUG("kthread_create failed.\n");
 		err = PTR_ERR(simple_thread);
 		simple_thread = NULL;
 		return err;
-    }
+    } else {
+		PDEBUG("kthread_create succ.\n");
+	}
 	return 0;
 }
 
